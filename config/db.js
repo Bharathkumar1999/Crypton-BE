41589bf218bf1fddae3ace017f3a972c1d5c3127
Bharath Kumar <bharathabdeviellers@gmail.comgit config --global user.email bharathabdeviellers@gmail.com>
2023-04-05 18:09:35 +0530

    new

--- config/db.js
+++ config/db.js
@@ -2,7 +2,12 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const connection= mongoose.connect(process.env.mongo_url,{ useNewUrlParser: true })
+const connection= mongoose.connect(process.env.mongo_url,{
+    //must add in order to not get any error masseges:
+    useUnifiedTopology:true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
 
 module.exports= {
     connection
